a452a9b990e0c73ccd44ece410cf982d6e12db9a
RestDayBlamage <restdayblamage@gmail.com>
2024-11-11 01:56:18 +0100

    Add files via upload

---
+++ next/script.js
@@ -0,0 +1,14 @@
+// Funkcja do otwarcia modala
+function openModal(element, title, date, location) {
+  document.getElementById("modal").style.display = "flex";
+  document.getElementById("modal-img").src = element.querySelector("img").src;
+  document.getElementById("modal-caption").innerHTML = `
+    <h3>${title}</h3>
+    <p>Data: ${date}</p>
+    <p>Location: ${location}</p>`;
+}
+
+// Funkcja do zamknięcia modala
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
