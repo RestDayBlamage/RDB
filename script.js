2d6e4e234dff9161587daea1ddbd51f7f75c3233
RestDayBlamage <restdayblamage@gmail.com>
2025-07-08 14:36:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -59,6 +59,18 @@ thumbnails.forEach((thumbnail, index) => {
 
 //------------------------------------------------------------------------------------------------------------   
 
+const scrollProgress = document.getElementById('scroll-progress');
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
+
+//------------------------------------------------------------------------------------------------------------   
+
 document.addEventListener('DOMContentLoaded', function() {
     const logo = document.getElementById('blmzLogo');
     if (!logo) return;
